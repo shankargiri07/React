3e8f9b299ac6cf14c8d5b6a82365802788c18115
shankar <shankargiri.Sl2000@gmail.com>
2023-08-23 10:07:36 +0530

    Redux Demo

---
+++ react-git/src/Redux-Demo/pages/home.jsx
@@ -0,0 +1,7 @@
+export function Home(){
+    return(
+        <div>
+            <h4>Home</h4>
+        </div>
+    )
+}
--- react-git/src/index.js
+++ react-git/src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <CookiesProvider>
-      <ReactRedux />
+      <App />
     </CookiesProvider>  
 </React.StrictMode>
 );
